e3ede521af19fc9d0dce1bef8832f43342738d26
Andrew Barrett <Andrew@MacBook-Pro.local>
2021-11-23 14:54:22 -0800

    Login system finished
    Tutorial completed, registration and login works.

--- reservation-app/server/app.js
+++ reservation-app/server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     user: "admin",
@@ -18,7 +20,36 @@ db.connect(function(err) {
       if (err) throw err;
       console.log(result);
     });
-  });
+});
+
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("INSERT INTO Login (username, password) VALUES (?,?)", 
+    [username, password],
+    (err, result) => {
+        console.log(err);
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("SELECT * FROM Login WHERE username = ? AND password = ?", 
+    [username, password],
+    (err, result) => {
+        if (err) {
+            res.send({err: err});
+        } 
+        if (result.length > 0) {
+            res.send(result);
+        } else {
+            res.send({message: "Wrong username/password combination!"});
+        }
+    });
+})
 
 app.listen(3001, () => {
     console.log("Running server");
