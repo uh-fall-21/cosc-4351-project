03f1edfa2df8772c3985add5274a168cbfd2e70f
themartinlopez <23004532+themartinlopez@users.noreply.github.com>
2021-11-29 16:01:51 -0600

    stuff

--- reservation-app/client/src/pages/Account.js
+++ reservation-app/client/src/pages/Account.js
@@ -1,14 +1,27 @@
 import React, { useContext } from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import { AuthContext } from "../helpers/AuthContext";
 
 function Account() {
+
+  let { id } = useParams();
+  const [username, setUsername] = useState("");
+
   const [listOfPosts, setListOfPosts] = useState([]);
   const [likedPosts, setLikedPosts] = useState([]);
   const { authState } = useContext(AuthContext);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+  const [newWage, setNewWage] = useState(0);
+  const [employeeList, setEmployeeList] = useState([]);
+
   let history = useHistory();
 
   useEffect(() => {
@@ -24,29 +37,166 @@ function Account() {
         });
     }
   }, []);
+/*
+  useEffect(() => {
+    axios.get(`http://localhost:3001/auth/basicinfo/${id}`).then((response) => {
+      setUsername(response.data.username);
+    });
 
+    axios.get(`http://localhost:3001/posts/byuserId/${id}`).then((response) => {
+      setListOfPosts(response.data);
+    });
+  }, []);  */
+///////////////////////////
+const addEmployee = () => {
+  axios.post("http://localhost:3001/create", {
+    name: name,
+    age: age,
+    country: country,
+    position: position,
+    wage: wage,
+  }).then(() => {
+    setEmployeeList([
+      ...employeeList,
+      {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        wage: wage,
+      },
+    ]);
+  });
+};
+
+const getEmployees = () => {
+  axios.get("http://localhost:3001/employees").then((response) => {
+    setEmployeeList(response.data);
+  });
+};
+
+const updateEmployeeWage = (id) => {
+  axios.put("http://localhost:3001/update", { wage: newWage, id: id }).then(
+    (response) => {
+      setEmployeeList(
+        employeeList.map((val) => {
+          return val.id == id
+            ? {
+                id: val.id,
+                name: val.name,
+                country: val.country,
+                age: val.age,
+                position: val.position,
+                wage: newWage,
+              }
+            : val;
+        })
+      );
+    }
+  );
+};
+
+const deleteEmployee = (id) => {
+  axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+    setEmployeeList(
+      employeeList.filter((val) => {
+        return val.id != id;
+      })
+    );
+  });
+};
+
+///////////////////////////
   return (
-    <div>
-      {listOfPosts.map((value, key) => {
-        return (
-          <div key={key} className="post">
-            <div className="title"> {value.title} </div>
-            <div
-              className="body"
-              onClick={() => {
-                history.push(`/post/${value.id}`);
-              }}
-            >
-              {value.postText}
-            </div>
-            <div className="footer">
-              <div className="username">
-                <Link to={`/profile/${value.UserId}`}> {value.username} </Link>
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        
+        <label>Address:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Billing Address:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+        <label>Preferred Dinner Number:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
+        <label>Points:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <label>Payment Method:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      <div className="employees">
+        <button onClick={getEmployees}>Show Employees</button>
+
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Country: {val.country}</h3>
+                <h3>Position: {val.position}</h3>
+                <h3>Wage: {val.wage}</h3>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="2000..."
+                  onChange={(event) => {
+                    setNewWage(event.target.value);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    updateEmployeeWage(val.id);
+                  }}
+                >
+                  {" "}
+                  Update
+                </button>
+
+                <button
+                  onClick={() => {
+                    deleteEmployee(val.id);
+                  }}
+                >
+                  Delete
+                </button>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
