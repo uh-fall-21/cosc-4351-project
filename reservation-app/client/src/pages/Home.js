b1f5f5ebbfefd7643209a45081499aaddbe7a9bf
themartinlopez <code.mlopez@gmail.com>
2021-11-28 21:36:26 -0600

    Incoming Replacement Files

---
+++ reservation-app/client/src/pages/Home.js
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import { AuthContext } from "../helpers/AuthContext";
+
+function Home() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState([]);
+  const { authState } = useContext(AuthContext);
+  let history = useHistory();
+
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      history.push("/login");
+    } else {
+      axios
+        .get("http://localhost:3001/posts", {
+          headers: { accessToken: localStorage.getItem("accessToken") },
+        })
+        .then((response) => {
+          setListOfPosts(response.data.listOfPosts);
+        });
+    }
+  }, []);
+
+  return (
+    <div>
+      {listOfPosts.map((value, key) => {
+        return (
+          <div key={key} className="post">
+            <div className="title"> {value.title} </div>
+            <div
+              className="body"
+              onClick={() => {
+                history.push(`/post/${value.id}`);
+              }}
+            >
+              {value.postText}
+            </div>
+            <div className="footer">
+              <div className="username">
+                <Link to={`/profile/${value.UserId}`}> {value.username} </Link>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
